91671333d8f7ac0aededf0f49938e81a5008a284
cleano01 <cleano.ferreira@dce.ufpb.br>
2020-11-09 22:08:35 -0300

    :arrows_clockwise: added truncate, to clean database for testing

--- __tests__/integration/teacher.test.js
+++ __tests__/integration/teacher.test.js
@@ -1,16 +1,18 @@
 const request = require('supertest');
 const app = require("../../app");
-
+const truncate = require("../util/truncate");
 
 describe('Teacher', ()=>{
+  beforeEach(async()=>{await truncate()})
+
   it('it is possible to register a teacher', async ()=>{
     const response = await request(app)
     .post('/')
     .send({
       name: "Rosa Maria",
-      email: "rosa02@gmail.com",
+      email: "rosa01@gmail.com",
       theme: "Português"
     });
-    expect(response.body.teacher.id).toEqual(1)    
-  })
+    expect(response.body.teacher).toHaveProperty('id');
+  });
 })
