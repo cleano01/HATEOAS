260df593a99474dc4d0419ba48d93466bde76799
cleano01 <cleano.ferreira@dce.ufpb.br>
2020-11-09 22:19:34 -0300

    :test_tube: added test case to check email duplicity in database

--- __tests__/integration/teacher.test.js
+++ __tests__/integration/teacher.test.js
@@ -15,4 +15,24 @@ describe('Teacher', ()=>{
     });
     expect(response.body.teacher).toHaveProperty('id');
   });
+
+  it('it will not be possible to register an email already registered', async ()=>{
+
+    await request(app)
+      .post('/')
+      .send({
+        name: "Rosa Maria",
+        email: "rosa01@gmail.com",
+        theme: "Português"
+    });    
+
+    const response = await request(app)
+    .post('/')
+    .send({
+      name: "Rosa Maria",
+      email: "rosa01@gmail.com",
+      theme: "Português"
+    });
+    expect(response.status).toBe(400);
+  });
 })
