6ff0f3081bd57329d88675098eb00b5cc65ba575
cleano01 <cleano.ferreira@dce.ufpb.br>
2021-07-31 22:31:15 -0300

    feature/common init common

--- __tests__/util/factories.js
+++ __tests__/util/factories.js
@@ -1,12 +1,11 @@
 const faker = require('faker');
-const {factory} = require('factory-girl');
+const { factory } = require('factory-girl');
 const Teacher = require('../../src/models/Teacher');
-
 
 factory.define('Teacher', Teacher, {
   name: faker.name.findName(),
   email: faker.internet.email(),
-  theme: "Português"
-})
+  theme: "Português",
+});
 
 module.exports = factory;
--- __tests__/util/truncate.js
+++ __tests__/util/truncate.js
@@ -2,10 +2,8 @@ const database = require("../../src/database/index");
 
 module.exports = async function truncate(models) {
   return await Promise.all(
-    Object.keys(database.sequelize.models).map(key=>{
-      return database.sequelize.models[key].destroy({ 
-        truncate: true, force: true
-      });
-    })
-  )    
+    Object.keys(database.sequelize.models).map((key) => database.sequelize.models[key].destroy({
+      truncate: true, force: true,
+    })),
+  );
 };
